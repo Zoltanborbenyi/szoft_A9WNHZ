2a326786f31813fc2ec85d63e260323a286d69bf
Zoltanborbenyi <borbenyiz222@gmail.com>
2024-03-02 23:36:43 +0100

    minden egyben

---
+++ WinFormsApp1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class Villogszamol : Button
+    {   
+
+        int szam;
+        Random rng = new Random();
+        public Villogszamol()
+        {
+            MouseLeave += Villogszamol_MouseLeave;
+            MouseEnter += Villogszamol_MouseEnter;
+            MouseClick += Villogszamol_MouseClick;
+            Width = 30;
+            Height = 30;
+        }
+
+        private void Villogszamol_MouseClick(object? sender, MouseEventArgs e)
+        {
+            if (szam==5)
+            {
+                szam = 0;
+                Text = szam.ToString();
+            }
+            else
+            {
+                szam++;
+                Text = szam.ToString();
+            }
+        }
+
+        private void Villogszamol_MouseEnter(object? sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(rng.Next(0, 244), rng.Next(0, 255), rng.Next(0, 255));
+        }
+
+        private void Villogszamol_MouseLeave(object? sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(rng.Next(0, 244), rng.Next(0, 255), rng.Next(0, 255));
+        }
+    }
+}
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -6,5 +6,26 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int sor = 0; sor < 10; sor++)
+            {
+                for (int oszlop = 0; oszlop < 10; oszlop++)
+                {
+                    Villogszamol gomb = new Villogszamol();
+                    gomb.Left = sor * 30;
+                    gomb.Top = oszlop * 30;
+                    if (oszlop < sor)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Controls.Add(gomb);
+                    }
+                }
+            }
+        }
     }
 }
